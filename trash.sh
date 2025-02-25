822b7cb1a0778166682cefda47c2e0d9f52e8546
Neosb <neosb@nuteksecurity.com>
2025-02-26 00:40:19 +0100

    MySQL Workbench

--- trash.sh
+++ trash.sh
@@ -18,21 +18,6 @@ is_container() {
     fi
 }
 
-# Check if running inside a container
-if is_container; then
-    echo "Running inside a container"
-    SUDO=""
-else
-    echo "Not running inside a container"
-    SUDO="sudo"
-fi
-
-echo "💛 Installing Homebrew... Or updating if already installed."
-$SUDO curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh | bash
-(echo; echo 'eval "$(/opt/homebrew/bin/brew shellenv)"') >> /Users/neosb/.zprofile
-eval "$(/opt/homebrew/bin/brew shellenv)"
-echo "💚 Homebrew is installed."
-
 # Cr$SUDO a Bash array (in this case, let's say we want to install some packages)
 packages_to_install=(
    "ollama" # - run LLM locally
@@ -105,11 +90,278 @@ packages_to_install=(
    "zap" # - web app scanner
 )
 
-# Print out the array to see what we're working with
-echo "Packages to install:"
-printf '%s\n' "${packages_to_install[@]}"
+# --- Use two indexed arrays ---
 
-not_installed=()
+declare -a package_names=()
+declare -a package_comments=()
+
+# Add packages and comments (keeping the indices in sync)
+package_names+=("ollama")
+package_comments+=("run LLM locally")
+
+package_names+=("podman")
+package_comments+=("docker alternative")
+
+package_names+=("act")
+package_comments+=("run GitHub workflows locally")
+
+package_names+=("neovim")
+package_comments+=("text editor")
+
+package_names+=("irssi")
+package_comments+=("irc client")
+
+package_names+=("dos2unix")
+package_comments+=("convert dos to unix")
+
+package_names+=("ipcalc")
+package_comments+=("ip calculator")
+
+package_names+=("subnetcalc")
+package_comments+=("ip calculator")
+
+package_names+=("expect")
+package_comments+=("automate interactive applications")
+
+package_names+=("fd")
+package_comments+=("find alternative")
+
+package_names+=("tmux")
+package_comments+=("terminal multiplexer")
+
+package_names+=("lsd")
+package_comments+=("ls alternative")
+
+package_names+=("bat")
+package_comments+=("cat alternative")
+
+package_names+=("ripgrep-all")
+package_comments+=("grep alternative")
+
+package_names+=("sd")
+package_comments+=("sed alternative")
+
+package_names+=("smap")
+package_comments+=("map network")
+
+package_names+=("nmap")
+package_comments+=("network scanner")
+
+package_names+=("p0f")
+package_comments+=("passive os fingerprinting")
+
+package_names+=("masscan")
+package_comments+=("port scanner")
+
+package_names+=("gobuster")
+package_comments+=("directory bruteforcer")
+
+package_names+=("feroxbuster")
+package_comments+=("directory bruteforcer")
+
+package_names+=("ffuf")
+package_comments+=("directory bruteforcer")
+
+package_names+=("nuclei")
+package_comments+=("vulnerability scanner")
+
+package_names+=("recon-ng")
+package_comments+=("reconnaissance framework")
+
+package_names+=("mitmproxy")
+package_comments+=("transparent proxy")
+
+package_names+=("metasploit")
+package_comments+=("red team toolbox")
+
+package_names+=("httpx")
+package_comments+=("http scanner")
+
+package_names+=("amass")
+package_comments+=("subdomain scanner")
+
+package_names+=("jq")
+package_comments+=("json parser")
+
+package_names+=("htmlq")
+package_comments+=("html parser")
+
+package_names+=("httrack")
+package_comments+=("website copier")
+
+package_names+=("monolith")
+package_comments+=("website copier")
+
+package_names+=("httpie")
+package_comments+=("curl alternative")
+
+package_names+=("glow")
+package_comments+=("markdown reader")
+
+package_names+=("ouch")
+package_comments+=("archive extractor")
+
+package_names+=("exploitdb")
+package_comments+=("exploit database")
+
+package_names+=("asciinema")
+package_comments+=("terminal recorder")
+
+package_names+=("agg")
+package_comments+=("ascii art generator")
+
+package_names+=("crunch")
+package_comments+=("wordlist generator")
+
+package_names+=("mdbook")
+package_comments+=("book generator")
+
+package_names+=("rustscan")
+package_comments+=("fast nmap scan (CTF)")
+
+package_names+=("sqlmap")
+package_comments+=("automated SQL Injection")
+
+package_names+=("bettercap")
+package_comments+=("network attacks")
+
+package_names+=("exiftool")
+package_comments+=("read/write exif metadata")
+
+package_names+=("powershell")
+package_comments+=("windows shell")
+
+package_names+=("pyenv")
+package_comments+=("python version manager")
+
+package_names+=("pyenv-virtualenv")
+package_comments+=("virtualenv for pyenv")
+
+package_names+=("rbenv")
+package_comments+=("ruby version manager")
+
+package_names+=("nvm")
+package_comments+=("node version manager")
+
+package_names+=("diffusionbee")
+package_comments+=("AI art generator")
+
+package_names+=("podman-desktop")
+package_comments+=("docker alternative")
+
+package_names+=("homebrew/cask/httpie")
+package_comments+=("curl alternative")
+
+package_names+=("pgadmin4")
+package_comments+=("database administration")
+
+package_names+=("mysqlworkbench")
+package_comments+=("database administration")
+
+package_names+=("imhex")
+package_comments+=("hex editor")
+
+package_names+=("warp")
+package_comments+=("terminal")
+
+package_names+=("alacritty")
+package_comments+=("terminal")
+
+package_names+=("kitty")
+package_comments+=("terminal")
+
+package_names+=("homebrew/cask/wireshark")
+package_comments+=("network analyzer")
+
+package_names+=("termshark")
+package_comments+=("wireshark alternative")
+
+package_names+=("burp-suite")
+package_comments+=("wab app proxy")
+
+package_names+=("font-hack-nerd-font")
+package_comments+=("font")
+
+package_names+=("zed")
+package_comments+=("text editor")
+
+package_names+=("homebrew/cask/visual-studio-code")
+package_comments+=("text editor")
+
+package_names+=("firefox")
+package_comments+=("web browser")
+
+package_names+=("zap")
+package_comments+=("web app scanner")
+
+
+# --- Display the numbered list ---
+
+echo "Packages to install:"
+for i in "${!package_names[@]}"; do
+  printf "%2d. %-30s %s\n" "$((i+1))" "${package_names[$i]}" "${package_comments[$i]}"
+done
+
+read -r -p "Enter the numbers of the packages you want to EXCLUDE (comma-separated, or 'all' to exclude all, or press Enter to install all): " exclude_list
+
+# --- Process the exclusion input ---
+
+if [[ "$exclude_list" == "all" ]]; then
+    echo "Excluding all packages. Exiting."
+    exit 0
+elif [[ -n "$exclude_list" ]]; then
+    exclude_indices=($(echo "$exclude_list" | tr ',' ' '))
+
+    # Create temporary arrays to hold the filtered lists
+    temp_names=()
+    temp_comments=()
+
+    for i in "${!package_names[@]}"; do
+        exclude=false
+        for idx in "${exclude_indices[@]}"; do
+            if [[ "$((i+1))" -eq "$idx" ]]; then
+                exclude=true
+                break
+            fi
+        done
+        if ! $exclude; then
+            temp_names+=("${package_names[$i]}")
+            temp_comments+=("${package_comments[$i]}")
+        fi
+    done
+
+    # Replace the original arrays with the filtered ones
+    package_names=("${temp_names[@]}")
+    package_comments=("${temp_comments[@]}")
+fi
+
+# --- Display packages to install after exclusion ---
+
+echo "Packages to install after exclusion:"
+printf '%s\n' "${package_names[@]}"
+
+# Ask the user to confirm before proceeding
+echo ""
+echo "💛 This script will install the above packages using Homebrew."
+echo "💛 Please review the list of packages to be installed and confirm..."
+
+read -n 1 -r -p "Continue installing above packages? (y/n) " choice
+echo ""  # Add a newline after the input
+
+case "$choice" in
+  y|Y)
+    echo "Continuing..."
+    # Commands to execute if the user enters 'y' or 'Y'
+    ;;
+  n|N)
+    echo "Exiting..."
+    exit 1  # Exit with a non-zero status code to indicate failure (optional)
+    ;;
+  *)
+    echo "Invalid input. Please enter 'y' or 'n'."
+    # You might want to loop back here to ask again
+    ;;
+esac
 
 # Install Rosetta 2
 if [[ "$(uname -s)" == "Darwin" ]] && [[ "$(arch)" == "arm64" ]]; then
@@ -117,11 +369,26 @@ if [[ "$(uname -s)" == "Darwin" ]] && [[ "$(arch)" == "arm64" ]]; then
     $SUDO softwareupdate --install-rosetta
 fi
 
+# Check if running inside a container
+if is_container; then
+    echo "Running inside a container"
+    SUDO=""
+else
+    echo "Not running inside a container"
+    SUDO="sudo"
+fi
+
+echo "💛 Installing Homebrew... Or updating if already installed."
+$SUDO curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh | bash
+(echo; echo 'eval "$(/opt/homebrew/bin/brew shellenv)"') >> /Users/neosb/.zprofile
+eval "$(/opt/homebrew/bin/brew shellenv)"
+echo "💚 Homebrew is installed."
+
 # Iterate over the array and install each package using brew
 # install from Homebrew, dnf, or apt
 # may stop for sudo password
 echo "💛 Installing selected programs..."
-for package in "${packages_to_install[@]}"; do
+for package in "${package_names[@]}"; do
     if [[ "$(uname -s)" == "Darwin" ]]; then
         # For each package, run a command like this: "brew install $package"
         echo "💛 Installing $package..."
@@ -143,23 +410,29 @@ for package in "${packages_to_install[@]}"; do
     fi
 done
 
-echo "💚 All installing done!"
+echo "💚 All installs done!"
 
 echo "💚 Configuration in ~/.zshrc"
-echo ""  >> ~/.zshrc
-echo "# ZSH completions" >> ~/.zshrc
-echo 'fpath=($fpath /opt/homebrew/share/zsh/site-functions)' >> ~/.zshrc
-echo "💚 Setup fpath for zsh completions ~/.zshrc..."
-echo ""  >> ~/.zshrc
-echo "# NVM" >> ~/.zshrc
-echo 'export NVM_DIR="$HOME/.nvm"' >> ~/.zshrc
-echo '[ -s "/opt/homebrew/opt/nvm/nvm.sh" ] && \. "/opt/homebrew/opt/nvm/nvm.sh"  # This loads nvm' >> ~/.zshrc
-echo '[ -s "/opt/homebrew/opt/nvm/etc/bash_completion.d/nvm" ] && \. "/opt/homebrew/opt/nvm/etc/bash_completion.d/nvm"  # This loads nvm bash_completion' >> ~/.zshrc
-source ~/.zshrc
-echo "💚 Setup nvm in ~/.zshrc..."
+if grep -qF "# ZSH completions" ~/.zshrc; then
+    echo "ZSH completions already in file"
+else
+    echo ""  >> ~/.zshrc
+    echo "# ZSH completions" >> ~/.zshrc
+    echo 'fpath=($fpath /opt/homebrew/share/zsh/site-functions)' >> ~/.zshrc
+    echo "💚 Setup fpath for zsh completions ~/.zshrc..."
+    source ~/.zshrc
+fi
 
-# Print out the array to see what we're working with
-echo "❤️ Packages not installed:"
-printf '%s\n' "${not_installed[@]}"
+if grep -qF "# NVM" ~/.zshrc; then
+    echo "NVM already in file"
+else
+    echo ""  >> ~/.zshrc
+    echo "# NVM" >> ~/.zshrc
+    echo 'export NVM_DIR="$HOME/.nvm"' >> ~/.zshrc
+    echo '[ -s "/opt/homebrew/opt/nvm/nvm.sh" ] && \. "/opt/homebrew/opt/nvm/nvm.sh"  # This loads nvm' >> ~/.zshrc
+    echo '[ -s "/opt/homebrew/opt/nvm/etc/bash_completion.d/nvm" ] && \. "/opt/homebrew/opt/nvm/etc/bash_completion.d/nvm"  # This loads nvm bash_completion' >> ~/.zshrc
+    echo "💚 Setup nvm in ~/.zshrc..."
+    source ~/.zshrc
+fi
 
-echo "🚀 Done! Press [Return ⏎] to exit"
+echo "❤️ Done! Press [Return ⏎] to exit"
